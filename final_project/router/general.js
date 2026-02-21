44a3a35db06edf343ba5d6d0a3d7a43c7c5e5d10
ArthurBondar <arthur.bondar.1@gmail.com>
2026-02-20 19:35:00 -0500

    task 7 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,21 +12,19 @@ public_users.post("/register", (req, res) => {
     if (!username || !password)
         return res.status(404).json({ message: "Error logging in" });
 
-    // Authenticate user
-    if (authenticatedUser(username, password)) {
-        // Generate JWT access token
-        let accessToken = jwt.sign({
-            data: password
-        }, 'access', { expiresIn: 60 * 60 });
-
-        // Store access token and username in session
-        req.session.authorization = {
-            accessToken, username
+    // Check if both username and password are provided
+    if (username && password) {
+        // Check if the user does not already exist
+        if (!isValid(username)) {
+            // Add the new user to the users array
+            users.push({ "username": username, "password": password });
+            return res.status(200).json({ message: "User successfully registered. Now you can login" });
+        } else {
+            return res.status(404).json({ message: "User already exists!" });
         }
-        return res.status(200).send("User successfully logged in");
-    } else {
-        return res.status(208).json({ message: "Invalid Login. Check username and password" });
     }
+    // Return error if username or password is missing
+    return res.status(404).json({ message: "Unable to register user." });
 });
 
 // Get the book list available in the shop
